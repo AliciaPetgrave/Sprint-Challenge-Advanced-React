acaafaba053bf3ad28f3fd098a7786f6dd24c6de
Alicia Petgrave <alicia.petgrave@gmail.com>
2020-02-14 13:01:58 -0500

    adding test

--- client/src/App.test.js
+++ client/src/App.test.js
@@ -1,9 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import { render } from "@testing-library/react";
+import NavBar from './Components/NavBar'
 
 it('renders without crashing', () => {
   const div = document.createElement('div');
   ReactDOM.render(<App />, div);
   ReactDOM.unmountComponentAtNode(div);
+});
+
+
+test("renders NavBar component without crashing", () => {
+  render(<NavBar />)
 });
